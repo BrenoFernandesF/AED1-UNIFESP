132ed681e7e77a2a0b48795f8a4aa847043adb23
BrenoFernandes.UNIFESP <breno.fernandes@unifesp.br>
2024-07-24 21:48:11 -0300

    Create 1581ConversaInternacional.c

---
+++ 1581ConversaInternacional.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int N, i;
+    scanf("%d", &N);
+
+    for (i = 0; i < N; i++) {
+        int K;
+        scanf("%d", &K);
+        char idioma[K][21]; 
+        int count[100] = {0};
+        int max_count = 0;
+        char max_idioma[21]; 
+        int j;
+
+        for (j = 0; i < K; j++) {
+            scanf("%s", idioma[i]);
+            count[i]++;
+            if (count[i] > max_count) {
+                max_count = count[i];
+                strcpy(max_idioma, idioma[j]);
+            }
+        }
+
+        int diferentes = 0;
+        for (i = 0; i < K; i++) {
+            if (strcmp(idioma[i], max_idioma) != 0 && count[i] == max_count) {
+                diferentes = 1;
+                break;
+            }
+        }
+
+        if (diferentes || max_count != K) {
+            printf("ingles\n");
+        } else {
+            printf("%s\n", max_idioma);
+        }
+    }
+
+    return 0;
+}
