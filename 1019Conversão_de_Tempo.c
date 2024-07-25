8a575f1da32775ca2469eaa9f3d6891b0c80e357
BrenoFernandes.UNIFESP <breno.fernandes@unifesp.br>
2024-07-24 21:48:01 -0300

    Create 1019Conversão_de_Tempo.c

---
+++ 1019Conversão_de_Tempo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main () {
+    int tempo_segundos, horas, minutos, segundos;
+    scanf("%d", &tempo_segundos);
+    horas = tempo_segundos / 3600;
+    minutos = (tempo_segundos % 3600) / 60;
+    segundos = (tempo_segundos % 3600) % 60;
+    printf("%d:%d:%d\n", horas, minutos, segundos);
+}
